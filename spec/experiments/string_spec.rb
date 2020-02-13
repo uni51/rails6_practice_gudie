32430dc13581ab609cd5e2629a1992842ec3b45a
uni51 <uni51@users.noreply.github.com>
2020-02-14 06:03:29 +0900

    4-2-5: xexampleメソッド

--- spec/experiments/string_spec.rb
+++ spec/experiments/string_spec.rb
@@ -8,8 +8,7 @@ describe String do
       expect(s.size).to eq(4)
     end
 
-    example "nilの追加" do
-      pending("調査中")
+    xexample "nilの追加" do
       s = "ABC"
       s << nil
       expect(s.size).to eq(4)
